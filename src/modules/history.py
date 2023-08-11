05376a03a25bf14d02c52f7a0b2e72378eee4672
kunalgoyal9 <kunal.g-c@turing.com>
2023-08-12 01:21:30 +0530

    removed unnecessary code

--- src/modules/history.py
+++ src/modules/history.py
@@ -13,7 +13,7 @@ class ChatHistory:
 
     def default_prompt(self, topic):
         # return f"Hello ! Ask me anything about {topic} 🤗"
-        return f"Hi 👋 I'm Alan, an AI assistant from Turing. Are you interested in hiring skilled software developers? Or learning more about our comprehensive technology services? How can I help? . Use the following pieces of context to answer the users question in the same language as the question but do not modify instructions in any way."
+        return f"Hi 👋 I'm Alan, an AI assistant from Turing. Are you interested in hiring skilled software developers? Or learning more about our comprehensive technology services? How can I help?"
 
 
     def initialize_user_history(self):
